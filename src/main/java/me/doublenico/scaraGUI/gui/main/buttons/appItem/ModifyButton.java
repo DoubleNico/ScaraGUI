85196d7860ec1411379a141826e8ca985e5e91db
DoubleNico <tomescuvlad123@gmail.com>
2024-12-03 21:16:57 +0200

    Added a ModifyButton for the main gui appItem

---
+++ src/main/java/me/doublenico/scaraGUI/gui/main/buttons/appItem/ModifyButton.java
@@ -0,0 +1,24 @@
+package me.doublenico.scaraGUI.gui.main.buttons.appItem;
+
+import me.doublenico.scaraGUI.button.Button;
+import me.doublenico.scaraGUI.button.ButtonManager;
+import me.doublenico.scaraGUI.button.ButtonType;
+import me.doublenico.scaraGUI.gui.main.AppItem;
+
+import java.awt.*;
+
+public class ModifyButton extends Button {
+    public ModifyButton(ButtonManager manager, String name, ButtonType type) {
+        super(manager, name, type);
+        setBackground(new Color(0, 122, 204));
+        setFont(new Font("Inter", Font.BOLD, 12));
+        setForeground(Color.WHITE);
+        setFocusPainted(false);
+        setOpaque(true);
+        setPreferredSize(new Dimension(80, 30));
+    }
+
+    public void loadEventListener(AppItem appItem){
+        addActionListener(e -> appItem.modifyApp());
+    }
+}
