c5fadac026c1ec6e3b76e9bd95c8421d5befdbd8
DoubleNico <tomescuvlad123@gmail.com>
2024-12-03 21:17:14 +0200

    Added a RefreshButton for the settings gui

---
+++ src/main/java/me/doublenico/scaraGUI/gui/settings/buttons/RefreshButton.java
@@ -0,0 +1,25 @@
+package me.doublenico.scaraGUI.gui.settings.buttons;
+
+import me.doublenico.scaraGUI.button.Button;
+import me.doublenico.scaraGUI.button.ButtonManager;
+import me.doublenico.scaraGUI.button.ButtonType;
+import me.doublenico.scaraGUI.gui.settings.SettingsGui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class RefreshButton extends Button {
+    public RefreshButton(ButtonManager manager, String name, ButtonType type) {
+        super(manager, name, type);
+        setFont(new Font("Inter", Font.BOLD, 12));
+        setBackground(new Color(204, 0, 0));
+        setForeground(Color.WHITE);
+        setFocusPainted(false);
+        setPreferredSize(new Dimension(100, 40));
+        setBorder(BorderFactory.createEmptyBorder());
+    }
+
+    public void loadEventListener(SettingsGui settings){
+        addActionListener(e -> settings.refreshDeviceList());
+    }
+}
