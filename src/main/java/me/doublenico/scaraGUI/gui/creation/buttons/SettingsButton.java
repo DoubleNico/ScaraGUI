96fa6144266f485c8181e40102bd78887fccb0a7
DoubleNico <tomescuvlad123@gmail.com>
2024-12-09 11:47:30 +0200

    Created a SettingsButton for the AppCreationGUI

---
+++ src/main/java/me/doublenico/scaraGUI/gui/creation/buttons/SettingsButton.java
@@ -0,0 +1,27 @@
+package me.doublenico.scaraGUI.gui.creation.buttons;
+
+import me.doublenico.scaraGUI.button.Button;
+import me.doublenico.scaraGUI.button.ButtonManager;
+import me.doublenico.scaraGUI.button.ButtonType;
+import me.doublenico.scaraGUI.gui.main.ScaraGUI;
+import me.doublenico.scaraGUI.utils.SettingsActionListenerUtils;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SettingsButton extends Button {
+
+    public SettingsButton(ButtonManager manager, String name, ButtonType type) {
+        super(manager, name, type);
+        setFont(new Font("Inter", Font.BOLD, 12));
+        setBackground(new Color(37, 41, 45));
+        setForeground(Color.WHITE);
+        setFocusPainted(false);
+        setPreferredSize(new Dimension(80, 30));
+        setBorder(BorderFactory.createEmptyBorder());
+    }
+
+    public void loadEventListener(ScaraGUI owner){
+        new SettingsActionListenerUtils(this).loadEventListener(owner);
+    }
+}
