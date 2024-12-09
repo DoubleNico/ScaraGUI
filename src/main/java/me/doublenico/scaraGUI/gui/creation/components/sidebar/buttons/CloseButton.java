58295d19c519e4d3bc8cbb948fc5ee1c7a2912d8
DoubleNico <tomescuvlad123@gmail.com>
2024-12-09 11:49:39 +0200

    Added a CloseButton for the Sidebar

---
+++ src/main/java/me/doublenico/scaraGUI/gui/creation/components/sidebar/buttons/CloseButton.java
@@ -0,0 +1,31 @@
+package me.doublenico.scaraGUI.gui.creation.components.sidebar.buttons;
+
+import me.doublenico.scaraGUI.button.Button;
+import me.doublenico.scaraGUI.button.ButtonManager;
+import me.doublenico.scaraGUI.button.ButtonType;
+import me.doublenico.scaraGUI.gui.creation.buttons.SidebarButton;
+import me.doublenico.scaraGUI.gui.creation.components.sidebar.CreationSidebar;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CloseButton extends Button {
+    public CloseButton(ButtonManager manager, String name, ButtonType type) {
+        super(manager, name, type);
+        setToolTipText("Close the application");
+        setFont(new Font("Inter", Font.BOLD, 12));
+        setBackground(new Color(204, 0, 0));
+        setForeground(Color.WHITE);
+        setFocusPainted(false);
+        setPreferredSize(new Dimension(40, 30));
+    }
+
+    public void loadEventListener(CreationSidebar sideBar, SidebarButton openSidebarButton, JPanel contentPane){
+        addActionListener(e -> {
+            sideBar.setVisible(false);
+            openSidebarButton.setVisible(true);
+            contentPane.revalidate();
+            contentPane.repaint();
+        });
+    }
+}
