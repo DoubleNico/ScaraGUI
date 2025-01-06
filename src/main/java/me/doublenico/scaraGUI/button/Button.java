0ea72413f91611c390a46015fd2172b52cbba738
DoubleNico <tomescuvlad123@gmail.com>
2025-01-07 00:51:24 +0200

    Added the ApplicationFrame to the Button wrapper

--- src/main/java/me/doublenico/scaraGUI/button/Button.java
+++ src/main/java/me/doublenico/scaraGUI/button/Button.java
@@ -1,16 +1,20 @@
 package me.doublenico.scaraGUI.button;
 
+import me.doublenico.scaraGUI.frame.ApplicationFrame;
+
 import javax.swing.*;
 import java.awt.*;
 
 public abstract class Button extends JButton implements IButton{
 
     private final String name;
+    private final ApplicationFrame  applicationFrame;
     private final ButtonType type;
 
-    public Button(ButtonManager manager, String name, ButtonType type) {
+    public Button(ButtonManager manager, ButtonType type, ApplicationFrame applicationFrame, String name){
         this.name = name;
         this.type = type;
+        this.applicationFrame = applicationFrame;
         setName(name);
         setText(name);
         manager.addButton(this);
@@ -20,6 +24,10 @@ public abstract class Button extends JButton implements IButton{
         setForeground(Color.WHITE);
         setFocusPainted(false);
         setOpaque(true);
+    }
+
+    public ApplicationFrame getApplicationFrame() {
+        return applicationFrame;
     }
 
     @Override
