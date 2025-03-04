94a7f84228e24aafe370f325bd5528a07860406f
DoubleNico <tomescuvlad123@gmail.com>
2025-03-04 14:02:15 +0200

    Added Joint3 and Acceleration to the CreationLabel

--- src/main/java/me/doublenico/scaraGUI/gui/creation/components/form/CreationLabel.java
+++ src/main/java/me/doublenico/scaraGUI/gui/creation/components/form/CreationLabel.java
@@ -6,9 +6,11 @@ public enum CreationLabel {
     NAME("Name", new NameHelpLabel("Help for Name"), "This will be the name of the operation", "Name", 0),
     JOINT1("Joint 1", new NameHelpLabel("Help for Joint1"), "Value of the first joint", "Angle", 1),
     JOINT2("Joint 2", new NameHelpLabel("Help for Joint2"), "Value of the second joint", "Angle", 2),
-    Z("Z", new NameHelpLabel("Help for Z"), "This is the position in Z", "Position", 3),
-    GRIPPER("Gripper", new NameHelpLabel("Help for Gripper"), "The gripper is the thing that holds the object, it can be open or closed", "Value", 4),
-    SPEED("Speed", new NameHelpLabel("Help for Speed"), "This is the speed of the motors", "Value", 5);
+    JOINT3("Joint 3", new NameHelpLabel("Help for Joint3"), "Value of the third joint", "Angle", 3),
+    Z("Z", new NameHelpLabel("Help for Z"), "This is the position in Z", "Position", 4),
+    GRIPPER("Gripper", new NameHelpLabel("Help for Gripper"), "The gripper is the thing that holds the object, it can be open or closed", "Value", 5),
+    SPEED("Speed", new NameHelpLabel("Help for Speed"), "This is the speed of the motors", "Value", 6),
+    ACCELERATION("Acceleration", new NameHelpLabel("Help for Speed"), "This is the acceleration of the motors", "Value", 7);
 
     public final String name;
     public final CreationHelp help;
