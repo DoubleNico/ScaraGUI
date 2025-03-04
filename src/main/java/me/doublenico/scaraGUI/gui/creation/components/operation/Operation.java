bdbf88f1e592bfcacae68fbce61d492602dc810c
DoubleNico <tomescuvlad123@gmail.com>
2025-03-04 14:01:44 +0200

    Added joint3 and acceleration to the Operation

--- src/main/java/me/doublenico/scaraGUI/gui/creation/components/operation/Operation.java
+++ src/main/java/me/doublenico/scaraGUI/gui/creation/components/operation/Operation.java
@@ -8,18 +8,22 @@ public class Operation {
     private final String name;
     private final int joint1;
     private final int joint2;
+    private final int joint3;
     private final int z;
     private final int gripper;
     private final int speed;
+    private final int acceleration;
 
-    public Operation(UUID uuid, String name, int joint1, int joint2, int z, int gripper, int speed) {
+    public Operation(UUID uuid, String name, int joint1, int joint2, int joint3, int z, int gripper, int speed, int acceleration) {
         this.uuid = uuid;
         this.name = name;
         this.joint1 = joint1;
         this.joint2 = joint2;
+        this.joint3 = joint3;
         this.z = z;
         this.gripper = gripper;
         this.speed = speed;
+        this.acceleration = acceleration;
     }
 
     public boolean isValidOperation(int defaultValue){
@@ -52,5 +56,13 @@ public class Operation {
 
     public UUID getUuid() {
         return uuid;
+    }
+
+    public int getJoint3() {
+        return joint3;
+    }
+
+    public int getAcceleration() {
+        return acceleration;
     }
 }
